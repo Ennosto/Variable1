788eedc8523697bd101f34324adc80ea42e68355
Ennosto <ennosto_me@mail.ru>
2024-08-06 20:52:10 +0300

    Add files via upload

---
+++ Variable1.py
@@ -0,0 +1,9 @@
+Kolvo_DZ =   12
+Kolvo_Hr = 1.5
+Name_Kurs = 'Python'
+Time_on_one_mission = Kolvo_Hr / Kolvo_DZ
+slovo_0 = 'Курс:'
+slovo_1 = 'всего задач:'
+slovo_2 = 'затрачено часов:'
+slovo_3 = 'среднее время выполнения:'
+print(slovo_0, Name_Kurs, ",", slovo_1, Kolvo_DZ,",", slovo_2, Kolvo_Hr,",", slovo_3, Time_on_one_mission)
